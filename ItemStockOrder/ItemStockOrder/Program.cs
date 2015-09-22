c3a03fc74c695d7d9c1a34cacc0a8c4d015bf3e9
Shinotricks <Shinotrix@gmail.com>
2015-09-22 15:22:41 +0200

    Inte längre spaghetti

--- ItemStockOrder/ItemStockOrder/Program.cs
+++ ItemStockOrder/ItemStockOrder/Program.cs
@@ -16,8 +16,66 @@ namespace ItemStockOrder
             //Console.WriteLine("Number of items in stock: " + stock.NumberOfItems);
             //Console.ReadKey();
 
-            while (true) { 
-                Console.WriteLine("Enter first number"); float n1 = float.Parse(Console.ReadLine()); Console.WriteLine("Enter second number"); float n2 = float.Parse(Console.ReadLine()); Console.WriteLine("Enter operator"); char o = char.Parse(Console.ReadLine()); var n3 = o == '+' ? n1 + n2 : o == '-' ? n1 - n2 : o == '*' ? n1 * n2 : o == '/' ? n2 == 0?0: n1 / n2 : 0; Console.WriteLine(n3);
+            while (true)
+            {
+                Console.WriteLine("Enter first number");
+                float n1 = float.Parse(Console.ReadLine());
+                Console.WriteLine("Enter second number");
+                float n2 = float.Parse(Console.ReadLine());
+                Console.WriteLine("Enter operator");
+                char o = char.Parse(Console.ReadLine());
+
+                float n3;
+
+                /*
+                switch (o)
+                {
+                    case '+':
+                        n3 = n1 + n2;
+                        break;
+                    case '-':
+                        n3 = n1 - n2;
+                        break;
+                    case '*':
+                        n3 = n1 * n2;
+                        break;
+                    case '/':
+                        if (n2 != 0)
+                        {
+                            n3 = n1 / n2;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                        break;
+
+                }*/
+
+                if (o == '+')
+                {
+                    n3 = n1 + n2;
+                }
+                else if (o == '-')
+                {
+                    n3 = n1 - n2;
+                }
+                else if (o == '*')
+                {
+                    n3 = n1 * n2;
+                }
+                else if (o == '/')
+                {
+                    if (n2 != 0)
+                        n3 = n1 / n2;
+                    else
+                        return;
+                }
+                else
+                    return;
+
+                //var n3 = o == '+' ? n1 + n2 : o == '-' ? n1 - n2 : o == '*' ? n1 * n2 : o == '/' ? n2 == 0 ? 0 : n1 / n2 : 0;
+                Console.WriteLine(n3);
             }
         }
     }
